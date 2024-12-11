4fa53ac445b829bf2de535b313e67f275a76186f
Testing Name <testing@email.com>
2024-12-10 21:31:30 -0500

    adds cpp file and exe

---
+++ testing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string message = "This program is for testing only";
+    int a = 42;
+    cout << message << endl;
+    cout << a << endl;
+
+    return 0;
+}
